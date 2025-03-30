040f90c737feb1d25f7c77f10e424808797da590
MuhammadAtlant <110223372+MuhammadAtlant@users.noreply.github.com>
2025-03-30 22:06:14 +0700

    Add files via upload

---
+++ preview_tabel_kf_inventory.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `rakamin-kf-analytics-454907.Kimia_Farma_Big_Data_Analyst.kf_inventory`limit 10;
---
+++ preview_tabel_kf_product.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `rakamin-kf-analytics-454907.Kimia_Farma_Big_Data_Analyst.kf_product` limit 10;
